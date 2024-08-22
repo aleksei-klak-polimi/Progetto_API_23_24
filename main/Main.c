ea8af9d337ca97ef3e01de8967f67e97f0594172
aleksei-klak-polimi <aleksei.klak@mail.polimi.it>
2024-08-22 11:47:04 +0200

    Added condition in load courier function to check if the order queue is empty

--- main/Main.c
+++ main/Main.c
@@ -1563,50 +1563,52 @@ int setupCourier(Courier *c){
 }
 
 void loadCourier(Courier *courier, recipiesMap *book, orderedItemQueue *ordersReady){
-    int currentCourierLoad = 0;
+    if(ordersReady->head != NULL){
+        int currentCourierLoad = 0;
 
-    orderedItemList *orderNode = ordersReady->head;
+        orderedItemList *orderNode = ordersReady->head;
 
-    int breaker = 0;
-    while(breaker == 0){
-        //Check if enough space in Courier
-        if(currentCourierLoad + orderNode->el->totalWeigth < courier->capacity){
-            //order fits in the courier
-            //Load orders sorted by weight descending then by time ascending
+        int breaker = 0;
+        while(breaker == 0){
+            //Check if enough space in Courier
+            if(currentCourierLoad + orderNode->el->totalWeigth < courier->capacity){
+                //order fits in the courier
+                //Load orders sorted by weight descending then by time ascending
 
-            ordersReady->head = ordersReady->head->next;
+                ordersReady->head = ordersReady->head->next;
 
-            currentCourierLoad += orderNode->el->totalWeigth;
+                currentCourierLoad += orderNode->el->totalWeigth;
 
-            //removing utilization from recipie book
-            decrementRecipieUtilization(book, orderNode->el->name);
+                //removing utilization from recipie book
+                decrementRecipieUtilization(book, orderNode->el->name);
 
 
-            if(courier->ordersHead == NULL || courier->ordersHead->el->totalWeigth < orderNode->el->totalWeigth || (courier->ordersHead->el->totalWeigth == orderNode->el->totalWeigth && courier->ordersHead->el->time > orderNode->el->time)){
-                //if no orders are in courier OR first order weighs less than the current order OR they weigh the same but
-                //the current order is fresher then replace the head with the current order
-                orderNode->next = courier->ordersHead;
-                courier->ordersHead = orderNode;
-            }
-            else{
-                orderedItemList *prev = NULL;
-                orderedItemList *current = courier->ordersHead;
+                if(courier->ordersHead == NULL || courier->ordersHead->el->totalWeigth < orderNode->el->totalWeigth || (courier->ordersHead->el->totalWeigth == orderNode->el->totalWeigth && courier->ordersHead->el->time > orderNode->el->time)){
+                    //if no orders are in courier OR first order weighs less than the current order OR they weigh the same but
+                    //the current order is fresher then replace the head with the current order
+                    orderNode->next = courier->ordersHead;
+                    courier->ordersHead = orderNode;
+                }
+                else{
+                    orderedItemList *prev = NULL;
+                    orderedItemList *current = courier->ordersHead;
 
-                int breaker = 0;
-                while(breaker == 0){
-                    if(current->el->totalWeigth < orderNode->el->totalWeigth || (current->el->totalWeigth == orderNode->el->totalWeigth && current->el->time > orderNode->el->time)){
-                        breaker = 1;
-                        orderNode->next = current;
-                        prev->next = orderNode;
-                    }
-                    else if(current->next == NULL){
-                        breaker = 1;
-                        current->next = orderNode;
-                        orderNode->next = NULL;
-                    }
-                    else{
-                        prev = current;
-                        current = current->next;
+                    int breaker = 0;
+                    while(breaker == 0){
+                        if(current->el->totalWeigth < orderNode->el->totalWeigth || (current->el->totalWeigth == orderNode->el->totalWeigth && current->el->time > orderNode->el->time)){
+                            breaker = 1;
+                            orderNode->next = current;
+                            prev->next = orderNode;
+                        }
+                        else if(current->next == NULL){
+                            breaker = 1;
+                            current->next = orderNode;
+                            orderNode->next = NULL;
+                        }
+                        else{
+                            prev = current;
+                            current = current->next;
+                        }
                     }
                 }
             }
